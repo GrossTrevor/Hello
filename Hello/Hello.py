a308dd31b405e2ecf7c89e46c8f021831cf13594
ttttj <ttttj@LUMBULL>
2022-12-16 00:39:52 -0500

    Add project files.

---
+++ Hello/Hello.py
@@ -0,0 +1,8 @@
+name = input("Hello. What is your name? ")
+print(f"It's nice to meet you, {name}. How old are you? ", end = "")
+
+age = input()
+print(f"I see that you are still quite young at only {age}.")
+
+residence = input("Where do you live? ")
+print(f"Wow! I've always wanted to go to {residence}. Thanks for chatting with me. Bye!")
